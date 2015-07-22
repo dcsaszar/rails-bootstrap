9811492414819ae16ac8f13c93a4dbc148e6351d
David Császár <dcsaszar@users.noreply.github.com>
2015-07-22 15:25:31 +0200

    Scrivito setup

---
+++ config/initializers/scrivito.rb
@@ -0,0 +1,10 @@
+Scrivito.configure do |config|
+  #
+  # Uncomment following lines in order to explicitly set the tenant and the API key.
+  # If not explicitly set, the tenant and the API key are obtained from the environment variables
+  # SCRIVITO_TENANT and SCRIVITO_API_KEY.
+  #
+  # config.tenant = 'my-tenant-123'
+  # config.api_key = 'secret'
+  #
+end
